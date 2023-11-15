e29905378f8847d50c7848c15e7019d31f0426b8
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-11-15 15:34:34 -0700

    .

--- player.gd
+++ player.gd
@@ -10,22 +10,26 @@ extends CharacterBody2D
 func _physics_process(delta):
 	apply_gravity(delta)
 	var input_axis = Input.get_axis("ui_left", "ui_right")
-	apply_horizontal_force(delta, input_axis)
-	apply_friction(delta, input_axis)
+	if is_moving(input_axis):
+		apply_acceleration(delta, input_axis)
+	else:
+		apply_friction(delta)
 	jump_check()
 	move_and_slide()
+	
+func is_moving(input_axis):
+	return input_axis != 0
 	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y = move_toward(velocity.y, max_fall_velocity, gravity * delta)
 		
-func apply_horizontal_force(delta, input_axis):
-	if input_axis !=0:
+func apply_acceleration(delta, input_axis):
+	if is_moving(input_axis):
 		velocity.x = move_toward(velocity.x, input_axis * max_velocity, acceleration * delta)	
 		
-func apply_friction(delta, input_axis):
-	if input_axis == 0:
-		velocity.x = move_toward( velocity.x, 0 ,friction * delta)
+func apply_friction(delta):
+	velocity.x = move_toward( velocity.x, 0, friction * delta)
 		
 func jump_check():
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
