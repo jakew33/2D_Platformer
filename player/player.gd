e6e44120355fd96e7188d49f7b228d4510b42299
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-11-22 14:32:20 -0700

    added screenshake

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const DustEffect = preload("res://effects/dust_effect.tscn")
+const JumpEffectScene = preload("res://effects/jump_effect.tscn")
 
 @export var acceleration = 512
 @export var max_velocity = 64
@@ -59,6 +60,7 @@ func jump_check():
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = -jump_force
+			Utils.instantiate_scene_on_world(JumpEffectScene, global_position)
 	if not is_on_floor():
 		@warning_ignore("integer_division", "integer_division", "integer_division")
 		if Input.is_action_just_released("ui_up") and velocity.y < -jump_force / 2:
@@ -83,4 +85,5 @@ func _on_drop_timer_timeout():
 
 func _on_hurt_box_hurt(hitbox, damage):
 	camera_2d.reparent(get_tree().current_scene)
+	Events.add_screenshake.emit(3, 0.25)
 	queue_free()
--- projectile.gd
+++ projectile.gd
@@ -1,6 +1,8 @@
 class_name Projectile
 extends Node2D
 
+const ExplosionEffectScene = preload("res://effects/explosion_effect.tscn")
+
 @export var speed = 250
 
 var velocity = Vector2.ZERO
@@ -19,8 +21,12 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 
 func _on_hit_box_body_entered(body):
+	Utils.instantiate_scene_on_world(ExplosionEffectScene, global_position)
+#	Events.add_screenshake.emit(0.25, 0.25)
 	queue_free()
 
 
 func _on_hit_box_area_entered(area):
+	Utils.instantiate_scene_on_world(ExplosionEffectScene, global_position)
+#	Events.add_screenshake.emit(0.25, 0.25)
 	queue_free()
