1ae45de136a64c6a4b52a73184fa4119fa7053f1
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-11-27 13:11:53 -0700

    .

--- enemies/plant_enemy.gd
+++ enemies/plant_enemy.gd
@@ -3,7 +3,7 @@ extends Node2D
 const EnemyBulletScene = preload ("res://enemies/enemy_bullet.tscn")
 
 @export var bullet_speed = 30
-@export var spread = 30
+@export var spread = 45
 
 @onready var stats = $Stats
 @onready var bullet_spawn_point = $BulletSpawnPoint
@@ -12,10 +12,10 @@ const EnemyBulletScene = preload ("res://enemies/enemy_bullet.tscn")
 func fire_bullet():
 	var bullet = Utils.instantiate_scene_on_world(EnemyBulletScene, bullet_spawn_point.global_position) as Projectile
 	var direction = global_position.direction_to(fire_direction.global_position)
-	bullet.rotation = direction.angle()
-	bullet.rotate(randf_range(-deg_to_rad (30/2), deg_to_rad(30/2)))
-	bullet.speed = bullet_speed
-	bullet.update_velocity()
+	var velocity = direction.normalized() * bullet_speed
+	velocity = velocity.rotated(randf_range(-deg_to_rad (30/2), deg_to_rad(30/2)))
+	bullet.velocity = velocity
+
 
 func _on_hurt_box_hurt(hitbox, damage):
 	stats.health -= damage
