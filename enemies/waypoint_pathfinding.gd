5c6c15af4d61fde80177eb8fda2e1629fb58282b
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-12-08 13:08:49 -0700

    .

--- enemies/waypoint_pathfinding.gd
+++ enemies/waypoint_pathfinding.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var waypoint_buffer_distance = 8
+
 var waypoints = []
 var target: Node2D
 var pathfinding_next_position: Vector2
@@ -7,7 +9,8 @@ var pathfinding_next_position: Vector2
 @onready var ray_cast_2d = $RayCast2D
 
 func _physics_process(delta):
-	target = MainInstances.player
+	if not MainInstances.player is Node2D: return
+	target = MainInstances.player.center
 	if not target is Node2D: return
 	
 	if can_see_target(global_position):
@@ -17,11 +20,11 @@ func _physics_process(delta):
 		if not waypoints.is_empty():
 			pathfinding_next_position = waypoints.front()
 			
-			if global_position.distance_to(waypoints.front()) <= 8:
-				waypoints.pop_front()
-				
 			if not can_see_target(waypoints.back()):
 				waypoints.append(target.global_position)
+			
+			if global_position.distance_to(waypoints.front()) <= 8:
+				waypoints.pop_front()
 		else:
 			if not can_see_target(global_position):
 				waypoints.append(target.global_position)
