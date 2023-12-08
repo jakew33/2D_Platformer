f3126934474dd373a898393a09a4c431729b27c5
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-12-08 12:49:05 -0700

    .

--- enemies/waypoint_pathfinding.gd
+++ enemies/waypoint_pathfinding.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
+var waypoints = []
+var target: Node2D
+var pathfinding_next_position: Vector2
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@onready var ray_cast_2d = $RayCast2D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	pass
+
+func can_see_target(from):
+	ray_cast_2d.global_position = from
+	ray_cast_2d.target_position = target.global_position - from
+	ray_cast_2d.force_raycast_update()
+	return not ray_cast_2d.is_colliding()
