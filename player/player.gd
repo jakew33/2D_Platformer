b28ace89161473dbd1e68293cd15a8914e1c9da1
Jacob Winfrey <jacob_winfrey@yahoo.com>
2023-12-09 14:04:45 -0700

    .

--- player/player.gd
+++ player/player.gd
@@ -42,9 +42,12 @@ func _physics_process(delta):
 		fire_rate_timer.start()
 		player_blaster.fire_bullet()
 		
-	if Input.is_action_pressed("fire_missile") and fire_rate_timer.time_left == 0:
+	if (Input.is_action_pressed("fire_missile") 
+	and fire_rate_timer.time_left == 0
+	and PlayerStats.missiles > 0):
 		fire_rate_timer.start()
 		player_blaster.fire_missile()
+		PlayerStats.missiles -= 1
 		
 func _exit_tree():
 	MainInstances.player = null
